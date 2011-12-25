23cfc3120e476037852f0553504fc3c71d0cd0a3
Rodrigo <rodrigo@Rodrigo-MacBook-Pro.local>
2011-12-25 13:20:40 -0200

    Added aspirator as ticket #75 requested.

--- Appirater.h
+++ Appirater.h
@@ -95,7 +95,7 @@ extern NSString *const kAppiraterDeclinedToRate;
  Users need to 'use' the same version of the app this many times before
  before they will be prompted to rate it.
  */
-#define APPIRATER_USES_UNTIL_PROMPT		20		// integer
+#define APPIRATER_USES_UNTIL_PROMPT		15		// integer
 
 /*
  A significant event can be anything you want to be in your app. In a
